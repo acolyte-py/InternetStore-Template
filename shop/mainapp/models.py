635df675ea9d99d9a1161c4cd8e1a131e3df9d97
Михаил <spaik.mironov@yandex.ru>
2021-05-08 13:15:31 +0700

    14

--- shop/mainapp/models.py
+++ shop/mainapp/models.py
@@ -2,9 +2,20 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
-
+from django.urls import reverse
 
 User = get_user_model()
+
+
+def get_product_url(obj, viewname):
+    ct_model = obj.__class__._meta.model_name
+    return reverse(
+        viewname,
+        kwargs={
+            'ct_model': ct_model,
+            'slug': obj.slug,
+        }
+    )
 
 
 class LatestProductsManager:
@@ -34,7 +45,6 @@ class LatestProductsManager:
 
 
 class LatestProducts:
-
     object = LatestProductsManager()
 
 
@@ -71,7 +81,6 @@ class Product(models.Model):
 
 
 class Notebook(Product):
-
     diagonal = models.CharField(max_length=255, verbose_name='Диагональ')
     display = models.CharField(max_length=255, verbose_name='Тип дисплея')
     processor_freq = models.CharField(
@@ -88,9 +97,11 @@ class Notebook(Product):
     def __str__(self):
         return '{} : {}'.format(self.category.name, self.title)
 
+    def get_absolute_url(self):
+        return get_product_url(self, 'product_detail')
+
 
 class Smartphone(Product):
-
     diagonal = models.CharField(max_length=255, verbose_name='Диагональ')
     display = models.CharField(max_length=255, verbose_name='Тип дисплея')
     resolution = models.CharField(
@@ -118,6 +129,9 @@ class Smartphone(Product):
 
     def __str__(self):
         return '{} : {}'.format(self.category.name, self.title)
+
+    def get_absolute_url(self):
+        return get_product_url(self, 'product_detail')
 
 
 class CartProduct(models.Model):
